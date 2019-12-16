d1e808dcc031a608a84e384201a34c30ba0bd4b7
felix <923783137@qq.com>
2019-12-16 17:30:32 +0800

    update TwoSum.py

--- algorithms/python/TwoSum.py
+++ algorithms/python/TwoSum.py
@@ -2,22 +2,31 @@
 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
 '''
 
-
 from typing import List
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for n in range(len(nums)):
-            p1 = n
-            p2 = len(nums) - 1
-            while p1 < p2:
-                if nums[p1] + nums[p2] == target:
-                    return [p1,p2]
-                else:
-                    p2 -= 1
 
+# 解1，时间复杂度O(n²)
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i,j]
+        return None
+                
+
+# 解2，时间复杂度O(n)
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for n,num in enumerate(nums):
+            if (target - num) in d:
+                return [d[target - num],n]
+            else:
+                d[num] = n
+        return None
 
 
 if __name__ == "__main__":
-    soultion = Solution()
-    print(soultion.twoSum([3,2,4],6))
+    soultion = Solution2()
+    print(soultion.twoSum([2,7,11,15],9))
