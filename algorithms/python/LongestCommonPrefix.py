dc3a0c8cad1192cebc93552107b4c82162b133f1
felix-PC <zhangfelixqiang@foxmail.com>
2020-02-25 15:33:39 +0800

    Add LongestCommonPrefix.py

--- algorithms/python/LongestCommonPrefix.py
+++ algorithms/python/LongestCommonPrefix.py
@@ -3,9 +3,18 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pass
+        if len(strs) == 0:
+            return ""
+        prefix = strs[0]
+        for i in range(1,len(strs)):
+            while strs[i].find(prefix) != 0:
+                print(strs[i].find(prefix))
+                prefix = prefix[:-1]
+                if prefix == 0:
+                    return ""
+        return prefix
 
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.longestCommonPrefix(["flower","flow","flight"]))
+    print(solution.longestCommonPrefix(["c","acc","ccc"]))
